fc807787ec52ac1472ffb6ea0407e96a385918db
Bradie Tilley <44430471+bradietilley@users.noreply.github.com>
2024-05-14 21:38:23 +0800

    chore: remove toRomanNumerals

--- src/Traits/HasOutputTo.php
+++ src/Traits/HasOutputTo.php
@@ -45,51 +45,6 @@ trait HasOutputTo
     }
 
     /**
-     * Get the Roman Numerals equivalent of this value.
-     *
-     * Not super great for values far beyond 1 million. Romans rarely used
-     * numerals for depicting such large numbers -- apparently often used
-     * words instead.
-     */
-    public function toRomanNumerals(): string
-    {
-        $num = $this->toInteger();
-
-        $map = [
-            'M̅' => 1_000_000,
-            'D̅' => 500_000,
-            'C̅' => 100_000,
-            'L̅' => 50_000,
-            'X̅' => 10_000,
-            'V̅' => 5_000,
-            'M' => 1_000,
-            'CM' => 900,
-            'D' => 500,
-            'CD' => 400,
-            'C' => 100,
-            'XC' => 90,
-            'L' => 50,
-            'XL' => 40,
-            'X' => 10,
-            'IX' => 9,
-            'V' => 5,
-            'IV' => 4,
-            'I' => 1,
-        ];
-
-        $roman = '';
-
-        foreach ($map as $romanNumeral => $value) {
-            while ($num >= $value) {
-                $roman .= $romanNumeral;
-                $num -= $value;
-            }
-        }
-
-        return $roman;
-    }
-
-    /**
      * Get the decimal portion of this number
      */
     public function extractDecimal(): static
